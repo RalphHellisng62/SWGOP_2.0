294dfb46aa92c297ecb6ff820e562408191744da
RalphHellisng62 <arenasrafael889@gmail.com>
2026-07-28 15:41:55 -0600

    Fix clave anon de Supabase

--- Frontend/src/services/supabaseClient.ts
+++ Frontend/src/services/supabaseClient.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://orchzfygjitiaoqigljhy.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_fWFtY2BeeetX3HinMMUboQ_gmWar9z0' //
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yY2h6ZnlnanRpYXlxamdsamh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxOTI5OTksImV4cCI6MjEwMDc2ODk5OX0.qE7jVoMziYZAwG2DYcWNmBVt5ayluj1L7ItUZUXRd8s' //
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
